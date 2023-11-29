bc8d61827d8cf95a64d2641745a94c0310a30a51
Rdbmax <rdbmax@gmail.com>
2023-11-29 14:04:33 +0100

    update roadmap and dependancies

--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -1,6 +1,36 @@
 "use client";
+import clsx from "clsx";
+
 import Modal from "@Components/Modal/Modal";
 import styles from "./roadmap.module.css";
+
+const ROADMAP: {
+  done: boolean;
+  text: string;
+}[] = [
+  { text: "Ajouter les illustrations des 3 dernieres cartes", done: true },
+  { text: "filtrer sur les reverse", done: true },
+  { text: "afficher la roadmap", done: true },
+  { text: "nom de domaine (www.151-exchange.com)", done: true },
+  { text: "authentification U", done: true },
+  { text: "base de donnée", done: true },
+  { text: "U peut définir une liste de ses cartes en double", done: true },
+  {
+    text: "U peut rendre sa collection de double public via une url",
+    done: true,
+  },
+  { text: "U peut définir une liste de cartes souhaitées", done: true },
+  { text: "U peut rendre sa liste de souhaits public via une url", done: true },
+  { text: "version mobile", done: false },
+  { text: "ISR pour les pages doubles et souhaits", done: false },
+  { text: "afficher les details d'une carte", done: false },
+  { text: "Design de la page 404", done: false },
+  { text: "Pouvoir supprimer ses pages publique", done: false },
+  { text: "Pouvoir re-générer les urls des pages publique", done: false },
+  { text: "Branding : logo, favicon, meta de partages", done: false },
+  { text: "Ajouter une snackbar pour confirmer les actions U", done: false },
+  { text: "Ajouter une légende pour les icones sur les cartes", done: false },
+];
 
 export default function RoadMap() {
   return (
@@ -10,32 +40,11 @@ export default function RoadMap() {
           Roadmap <span className={styles.legend}>U = Utilisateur</span>
         </h2>
         <ul className={styles.roadmapList}>
-          <li className={styles.done}>
-            Ajouter les illustrations des 3 dernieres cartes
-          </li>
-          <li className={styles.done}>filtrer sur les reverse</li>
-          <li className={styles.done}>afficher la roadmap</li>
-          <li className={styles.done}>nom de domaine (www.151-exchange.com)</li>
-          <li className={styles.done}>authentification U</li>
-          <li className={styles.done}>base de donnée</li>
-          <li className={styles.done}>
-            U peut définir une liste de ses cartes en double
-          </li>
-          <li className={styles.done}>
-            U peut rendre sa collection de double public via une url
-          </li>
-          <li>U peut définir une liste de cartes souhaitées</li>
-          <li className={styles.done}>
-            U peut rendre sa liste de souhaits public via une url
-          </li>
-          <li>afficher les details d&apos;une carte</li>
-          <li>version mobile</li>
-          <li>Design de la page 404</li>
-          <li>Pouvoir supprimer ses pages publique</li>
-          <li>Pouvoir re-générer les urls des pages publique</li>
-          <li>Branding : logo, favicon, meta de partages</li>
-          <li>Ajouter une snackbar pour confirmer les actions U</li>
-          <li>Ajouter une légende pour les icones sur les cartes</li>
+          {ROADMAP.map(({ text, done }) => (
+            <li key={text} className={clsx(done && styles.done)}>
+              {text}
+            </li>
+          ))}
         </ul>
       </>
     </Modal>
