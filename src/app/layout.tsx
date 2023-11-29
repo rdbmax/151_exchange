c6973953cd9d85dc550a30fd70195dcf6d867127
Rdbmax <rdbmax@gmail.com>
2023-11-29 23:04:09 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Providers } from "@Components/provider";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="fr">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
